957b4c4b2051409cb3cf6a6a253826f8066f7cb8
Denny Wibowo <indocoffee@gmail.com>
2023-02-08 10:36:41 -0500

    initial commit

---
+++ src/main/java/com/dennywibowo/urlshorteningapi/UrlShorteningApiApplication.java
@@ -0,0 +1,13 @@
+package com.dennywibowo.urlshorteningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorteningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorteningApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dennywibowo/urlshorteningapi/UrlShorteningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennywibowo.urlshorteningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorteningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
