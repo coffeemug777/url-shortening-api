b179514e6820beca974df27815a65e6ef3e0ea4f
Denny Wibowo <indocoffee@gmail.com>
2023-02-08 13:49:07 -0500

    Add unit tests

--- src/test/java/com/dennywibowo/urlshorteningapi/UrlShorteningApiApplicationTests.java
+++ src/test/java/com/dennywibowo/urlshorteningapi/UrlShorteningApiApplicationTests.java
@@ -1,13 +1,65 @@
 package com.dennywibowo.urlshorteningapi;
 
+import com.dennywibowo.urlshorteningapi.controller.UrlShorteningController;
+import com.dennywibowo.urlshorteningapi.dto.UrlDataRequest;
+import com.dennywibowo.urlshorteningapi.dto.UrlDataResponse;
+import com.dennywibowo.urlshorteningapi.service.UrlDataService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UrlShorteningApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void UrlDataService_createUrl_Test() {
+		UrlDataService testService = new UrlDataService();
+		UrlDataRequest testRequest = new UrlDataRequest();
+		testRequest.setUrl("http://test.org/some/page/1");
+
+		UrlDataResponse result = testService.createUrl(testRequest);
+
+		assertAll("url",
+				() -> assertEquals(1, result.getId()),
+				() -> assertEquals("http://test.org/some/page/1", result.getUrl()),
+				() -> assertEquals(7, result.getShortUrl().length())
+		);
+
+		UrlDataResponse result2 = testService.createUrl(testRequest);
+
+		assertEquals(2, result2.getId());
+
+	}
+
+	@InjectMocks
+	private UrlShorteningController controller;
+
+	@Mock
+	private UrlDataService urlDataService;
+
+	@Test
+	void UrlShorteningController_test() {
+		UrlDataRequest testRequest = new UrlDataRequest();
+		testRequest.setUrl("http://test.org/some/page/1");
+		UrlDataResponse newResponse= UrlDataResponse.builder()
+				.id(1L)
+				.url("http://test.org/some/page/1")
+				.shortUrl("1234567")
+				.build();
+
+		when(urlDataService.createUrl(testRequest)).thenReturn(newResponse);
+
+		UrlDataResponse result =  controller.createUrl(testRequest);
+
+		verify(urlDataService, Mockito.times(1)).createUrl(testRequest);
+		assertEquals(newResponse, result);
 	}
 
 }
